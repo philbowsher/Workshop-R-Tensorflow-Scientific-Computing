5ce2e13c0bdd0150e3c85687b898e9809d23aea4
Phil Bowsher <phil@rstudio.com>
2021-02-18 18:46:59 +0000

    add shiny reticulate apps

---
+++ part1/reticulated-shiny-app-py2/server.R
@@ -0,0 +1,24 @@
+library(shiny)
+library(reticulate)
+
+source_python('server.py')
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+  # Expression that generates a histogram. The expression is
+  # wrapped in a call to renderPlot to indicate that:
+  #
+  #  1) It is "reactive" and therefore should be automatically
+  #     re-executed when inputs change
+  #  2) Its output type is a plot
+
+  output$distPlot <- renderPlot({
+    x    <- py$data
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
+
+})
