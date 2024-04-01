84cced87934bf8828685bcc81872909025a26b08
philbowsher <indyprb@gmail.com>
2024-04-01 21:22:00 +0000

    update pin deploy

--- part_3/immunotherapy/2_publish_model.R
+++ part_3/immunotherapy/2_publish_model.R
@@ -1,11 +1,18 @@
+# for this to work, be sure to go to Tools - Global Options - Publishing - Connect, and follow the instructions
+# And then establish connection to Posit Connect Server
+# https://pins.rstudio.com/reference/board_connect.html
+
+# This will take the tensorflow model and pin it to PC and the model will only available
+# to people or process with access
+
 library(pins)
-library(keras)
-# Pin it on RStudio Connect
-pins::board_register_rsconnect()
+# Pin it on Posit Connect
+con <- pins::board_connect()
+#make sure this was saved to wd for part 3
 local_zipfile <- zip::zip("model.zip", "saved_model")
 pins::pin(
   local_zipfile,
   "peptide_model",
   "Peptide Prediction Model",
-  "rsconnect"
+  con
 )
